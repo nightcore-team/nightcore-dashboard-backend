fd8797772aa452383f8d097421182bcd06c2faab
max <merzlyakov029@gmail.com>
2026-04-28 16:34:47 +0500

    feat: add ConfigTypeEnum

--- src/infra/postgres/models/_enums.py
+++ src/infra/postgres/models/_enums.py
@@ -1,10 +1,8 @@
 from enum import Enum
 
 
-class MetaConfigAccessTypeEnum(Enum):
-    """Enumeration for meta config access types."""
-
-    OTHER = "other"
+class ConfigTypeEnum(Enum):
+    FORUM = "forum"
     LOGGING = "logging"
     ECONOMY = "economy"
     LEVELS = "levels"
@@ -13,24 +11,10 @@ class MetaConfigAccessTypeEnum(Enum):
     MODERATION = "moderation"
     NOTIFICATIONS = "notifications"
     INFOMAKER = "infomaker"
-
-    @classmethod
-    def all_values(cls) -> list[str]:
-        """Get all enum values."""
-        return [choice.value for choice in cls]
-
-    @classmethod
-    def from_choice(cls, choice: str) -> "MetaConfigAccessTypeEnum":
-        """Get enum member from choice string."""
-        mapping = {
-            "clans": cls.CLANS,
-            "economy": cls.ECONOMY,
-            "levels": cls.LEVELS,
-            "logging": cls.LOGGING,
-            "moderation": cls.MODERATION,
-            "notifications": cls.NOTIFICATIONS,
-            "private_channels": cls.PRIVATE_CHANNELS,
-            "other": cls.OTHER,
-            "infomaker": cls.INFOMAKER,
-        }
-        return mapping[choice]
+    FAQ = "faq"
+    RULES = "rules"
+    PROPOSAL = "proposal"
+    ORG_ROLES = "org_roles"
+    MULTIPLERS = "multiplers"
+    TICKETS = "tickets"
+    COMPONENT_BUILDER = "component_builder"
